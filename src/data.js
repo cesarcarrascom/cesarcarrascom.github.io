8bd4fd2efeee0d6b392982cdbe9f85ece63466b3
Cesar Carrasco <carrascomcesar@gmail.com>
2021-10-29 12:36:00 -0300

    Timestamps added

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const data = [
     title: "Jobs Posting API",
     url: "https://github.com/cesarcarrascom/jobs-api",
     description:
-      "In this project I build a microservice where you can register as a company looking for employers. It features helmet security, JWT authentication, sorting and filtering, express routing middlewares, MongoDB connection and users validation.",
+      "In this project I use Express to build a microservice where you can register as a company looking for employers. It features helmet security, JWT authentication, sorting and filtering, express routing middlewares, MongoDB connection and users validation.",
     tags: ["Javascript", "NodeJS", "REST API"],
   },
   {
@@ -12,8 +12,16 @@ const data = [
     title: "Store Products API",
     url: "https://github.com/cesarcarrascom/store-api",
     description:
-      "A simple API where you can add, update and delete products in a store. Also, functionality to sort and filter all products by name, price and rating.",
+      "A starter API buit with Express where you can add, update and delete products in a store. Also, functionality to sort and filter all products by name, price and rating.",
     tags: ["Javascript", "NodeJS", "REST API"],
+  },
+  {
+    id: 3,
+    title: "Timestamps Microservice API",
+    url: "https://github.com/cesarcarrascom/timestamp",
+    description:
+      "A simple API where you can search for a specific date using 'yyyy-mm-dd' format.",
+    tags: ["Javascript", "NodeJS", "Timestamps"],
   },
 ];
 
