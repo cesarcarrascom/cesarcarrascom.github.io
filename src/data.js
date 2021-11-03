c032f6d85cf13f3c1f4ed94f39ffcf84d27e5054
Cesar Carrasco <carrascomcesar@gmail.com>
2021-11-03 11:32:36 -0300

    Added ecommerce react

--- src/data.js
+++ src/data.js
@@ -1,6 +1,22 @@
 const data = [
   {
     id: 1,
+    title: "Shop e-commerce API",
+    url: "https://github.com/cesarcarrascom/ecommerce-shop",
+    description:
+      "In this NodeJS app, I build a fully functional Express server and REST API for a e-commerce store. User register and login using JWT, create, update, delete, sort and filter products in a store. MongoDB database, cart and order checkout using Stripe.",
+    tags: ["Javascript", "NodeJS", "REST API", "JWT", "MongoDB"],
+  },
+  {
+    id: 2,
+    title: "Shop e-commerce frontend",
+    url: "https://cesarcarrascom.github.io/shop/",
+    description:
+      "In this React app, I build an e-commerce store frontend. It's a work in progress since I'm making the connections to my NodeJS API.",
+    tags: ["Javascript", "React", "Redux"],
+  },
+  {
+    id: 3,
     title: "Jobs Posting API",
     url: "https://github.com/cesarcarrascom/jobs-api",
     description:
@@ -8,15 +24,7 @@ const data = [
     tags: ["Javascript", "NodeJS", "REST API"],
   },
   {
-    id: 2,
-    title: "Store Products API",
-    url: "https://github.com/cesarcarrascom/store-api",
-    description:
-      "A starter API buit with Express where you can add, update and delete products in a store. Also, functionality to sort and filter all products by name, price and rating.",
-    tags: ["Javascript", "NodeJS", "REST API"],
-  },
-  {
-    id: 3,
+    id: 4,
     title: "Timestamps Microservice API",
     url: "https://github.com/cesarcarrascom/timestamp",
     description:
